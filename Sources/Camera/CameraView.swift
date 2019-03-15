eb08506ff856844b907668c73715b83c54397f40
Jack Chen <jack@flairtime.com>
2019-03-15 03:23:05 -0400

    Rename State to ButtonState

--- Sources/Camera/CameraView.swift
+++ Sources/Camera/CameraView.swift
@@ -175,10 +175,10 @@ class CameraView: UIView, UIGestureRecognizerDelegate {
   }
 
   func makeFlashButton() -> TripleButton {
-    let states: [TripleButton.State] = [
-      TripleButton.State(title: "Gallery.Camera.Flash.Off".g_localize(fallback: "OFF"), image: Bundle.image("gallery_camera_flash_off")!),
-      TripleButton.State(title: "Gallery.Camera.Flash.On".g_localize(fallback: "ON"), image: Bundle.image("gallery_camera_flash_on")!),
-      TripleButton.State(title: "Gallery.Camera.Flash.Auto".g_localize(fallback: "AUTO"), image: Bundle.image("gallery_camera_flash_auto")!)
+    let states: [TripleButton.ButtonState] = [
+      TripleButton.ButtonState(title: "Gallery.Camera.Flash.Off".g_localize(fallback: "OFF"), image: Bundle.image("gallery_camera_flash_off")!),
+      TripleButton.ButtonState(title: "Gallery.Camera.Flash.On".g_localize(fallback: "ON"), image: Bundle.image("gallery_camera_flash_on")!),
+      TripleButton.ButtonState(title: "Gallery.Camera.Flash.Auto".g_localize(fallback: "AUTO"), image: Bundle.image("gallery_camera_flash_auto")!)
     ]
 
     let button = TripleButton(states: states)
